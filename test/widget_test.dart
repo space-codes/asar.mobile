a7743e1792d38f237169e88cb85a09d337190f3e
gemydev <mohamed_gamal1146@yahoo.com>
2022-04-03 15:06:07 +0200

    try fix github action error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,6 +15,8 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(home: SplashScreen(),));
 
+    Future.delayed(Duration.zero, () {tester.tap(find.text('GO'));});
+
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
