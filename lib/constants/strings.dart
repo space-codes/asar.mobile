5651a0787f27dab373e08bd81f895117a5f144e3
gemydev <mohamed_gamal1146@yahoo.com>
2022-07-03 00:06:22 +0200

    fixed action

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -3,4 +3,7 @@
 // const String baseUrl = "http://127.0.0.1:5000/api/";
 
 
-const String baseUrl ="https://asar.loca.lt/api/";
+// https://asar.loca.lt
+// http://asar.eastasia.cloudapp.azure.com
+
+const String baseUrl ="http://asar.eastasia.cloudapp.azure.com/api/";
